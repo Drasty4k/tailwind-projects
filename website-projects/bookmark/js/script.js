09e428b2fd1c611053e5633fc4cf99c8c714c08c
Dragos Barbuta <barbuta.dragos12@gmail.com>
2022-09-09 17:18:29 +0300

    finished Mobile Menu for the Bookmark project

--- website-projects/bookmark/js/script.js
+++ website-projects/bookmark/js/script.js
@@ -7,6 +7,9 @@ const logo = document.getElementById("logo");
 // Tabs menu event listener
 tabs.forEach((tab) => tab.addEventListener("click", onTabClick));
 
+// Hamburger button listener
+btn.addEventListener("click", navToggle);
+
 function onTabClick(e) {
   // Deactivate all tabs
   tabs.forEach((tab) => {
@@ -28,4 +31,16 @@ function onTabClick(e) {
     .getElementById("panels")
     .getElementsByClassName(classString)[0]
     .classList.remove("hidden");
+}
+
+function navToggle() {
+  btn.classList.toggle("open");
+  menu.classList.toggle("flex");
+  menu.classList.toggle("hidden");
+
+  if (menu.classList.contains("flex")) {
+    logo.setAttribute("src", "./images/logo-bookmark-footer.svg");
+  } else {
+    logo.setAttribute("src", "./images/logo-bookmark.svg");
+  }
 }
